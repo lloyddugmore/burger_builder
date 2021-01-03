2e8d31393eaa2edecc3ff38bdfe26b3a41a68592
Lloyd <dugmore.lloyd@gmail.com>
2021-01-03 15:06:33 +1300

    auth guards added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
@@ -17,17 +17,38 @@ class App extends Component {
   }
 
   render () {
-    return (
-      <Layout>
+
+    let routes = (
+      <Switch>
+          <Route path="/auth" component={Auth}></Route>
+          <Route path="/" exact component={BurgerBuilder}></Route>
+          <Redirect to="/"/>
+      </Switch>
+    );
+
+    if (this.props.isAuth){
+      routes = (
         <Switch>
           <Route path="/checkout" component={Checkout}></Route>
           <Route path="/orders" component={Orders}></Route>
-          <Route path="/auth" component={Auth}></Route>
           <Route path="/logout" component={Logout}></Route>
           <Route path="/" exact component={BurgerBuilder}></Route>
+          <Redirect to="/"/>
         </Switch>
+      );
+    };
+
+    return (
+      <Layout>
+          {routes}
       </Layout>
     )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuth: state.auth.idToken !== null
   }
 }
 
@@ -37,4 +58,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
