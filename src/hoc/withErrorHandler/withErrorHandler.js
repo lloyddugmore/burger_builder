732fca26918d274e18a8e76221daeae96dbb8293
Lloyd <dugmore.lloyd@gmail.com>
2021-01-08 12:32:38 +1300

    added a custom hook

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,42 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../../hoc/Auxilliary';
+import useAxiosErrorHandler from '../../hooks/http-error-handler';
 
 const withErrorHandler = (WrappedComponent, axios) => {
    return props => {
-        
-        const [error, setError] = useState(null);
-
-        // want to clear the errors object on every request
-        const reqInterceptor = axios.interceptors.request.use(req => {
-            setError(null);
-            return req;
-        });
-
-        // want to set the errors object if there are errores on the response
-        // NOTE - res => res is the shortest way of returning the response.
-        const respInterceptor = axios.interceptors.response.use(res => res, err => {
-            setError(err);
-        });
-        
-
-        //When using a HOC component like this, and using interceptors, its best practice to
-        //tidy up the interceptors. Otherwise you could eventually get a memory leak of sorts.
-        useEffect(() => {
-            return () => {
-                axios.interceptors.request.eject(reqInterceptor);
-                axios.interceptors.request.eject(respInterceptor);
-            }
-        }, [reqInterceptor, respInterceptor]);
-
-        const errorConfirmedHandler = () => {
-            setError(null);
-        };
+        const [error, clearError] = useAxiosErrorHandler(axios)
 
         return (
             <Aux>
-                <Modal show={error} modalClosed={errorConfirmedHandler}>
+                <Modal show={error} modalClosed={clearError}>
                     {error 
                         ? <p>{error.message}</p> 
                         : null 
---
+++ src/hooks/http-error-handler.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+
+export default axiosClient => {
+    const [error, setError] = useState(null);
+
+    // want to clear the errors object on every request
+    const reqInterceptor = axiosClient.interceptors.request.use(req => {
+        setError(null);
+        return req;
+    });
+
+    // want to set the errors object if there are errores on the response
+    // NOTE - res => res is the shortest way of returning the response.
+    const respInterceptor = axiosClient.interceptors.response.use(res => res, err => {
+        setError(err);
+    });
+    
+
+    //When using a HOC component like this, and using interceptors, its best practice to
+    //tidy up the interceptors. Otherwise you could eventually get a memory leak of sorts.
+    useEffect(() => {
+        return () => {
+            axiosClient.interceptors.request.eject(reqInterceptor);
+            axiosClient.interceptors.request.eject(respInterceptor);
+        }
+    }, [reqInterceptor, respInterceptor]);
+
+    const errorConfirmedHandler = () => {
+        setError(null);
+    };
+
+    return [error, errorConfirmedHandler];
+}
